934527060a96df3c244e92c0ecb177fba4927122
Mario Garrido <mario.garrido@kuona.co>
2025-06-29 18:25:56 +0000

    Removed invalid argument

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,7 +10,7 @@ git flow init -d -t v
 git config gitflow.branch.master main
 git config core.hooksPath .githooks
 
-uv run pre-commit install --active
+uv run pre-commit install
 git add .
 git commit -m 'Initializing project.' --no-verify
 
