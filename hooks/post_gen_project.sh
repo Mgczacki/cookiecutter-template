e7ce5cc167694f9364fd43824f5c7e95785168c3
Mario Garrido <mario.garrido@kuona.co>
2025-06-29 10:35:40 +0000

    Modernize

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
 set -e
-
+pip install uv
+uv pip install pre-commit
+uv pip install gitflow
+git init
+git flow init -d -t v
 git config core.hooksPath .githooks
 
-pip install uv
-
-uv venv
-uv sync
-git init
 uv run pre-commit install
 git add .
 git commit -m 'Initializing project.' --no-verify
-git flow init -d -t v
+
