cce792a7a04a97f7ba5e74ca2c9040044e2310c8
Mario Garrido <mario.garrido@kuona.co>
2025-06-29 11:00:10 +0000

    Reverting bad changes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,12 +3,14 @@
 set -e
 pip install uv
 uv pip install pre-commit
+uv pip install hatch
 git init
 git branch -m main
 git flow init -d -t v
+git config gitflow.branch.master main
 git config core.hooksPath .githooks
 
-uv run pre-commit install
+uv run pre-commit install --active
 git add .
 git commit -m 'Initializing project.' --no-verify
 
