da88b59f9b52db56a869db59247f53444efdb7f1
Mario Garrido <mario.garrido@kuona.co>
2025-09-17 06:43:30 +0000

    Gitflow start update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -12,9 +12,14 @@ uv run pre-commit install
 cp .githooks/post-checkout .git/hooks/post-checkout
 echo "Custom post-checkout hook installed into .git/hooks."
 chmod +x .git/hooks/post-checkout
+
+git switch main
+
 git add .
 git commit -m 'Initializing project.' --no-verify
 if [ "{{ cookiecutter.force_push_to_remote }}" = "Y" ] && [ "{{ cookiecutter.remote_origin_url }}" != "https://git@github.com:user_name/repository_name.git" ]; then
     # Force push the main branch if a valid remote URL is provided and force_push_to_remote is enabled.
     git push -u origin main --force
+    git switch develop || git switch -c develop
+    git push -u origin develop || true
 fi
